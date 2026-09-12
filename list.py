10aa7587ad7cd59a3a8ce0882d3bea5312e57ed4
unknown <shravanpitla180@gmail.com>
2026-09-12 12:28:53 +0530

    list

---
+++ list.py
@@ -0,0 +1,22 @@
+#list concactination
+a=[10,20,30]
+b=[40,50,60]
+n= a + b
+print(n)
+
+#list repitation
+a=[10,20]
+print(a*3)
+
+#built.in functions
+a=[20,40,60,10,30,80,96,45]
+print(max(a))#maax()
+print(min(a))#min()
+print(len(a))#len()
+print(sorted(a))#sorted()
+
+#list methods
+# 1.append()
+a=[10,20,30]
+a.append(40)
+print(a)
