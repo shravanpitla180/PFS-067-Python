6ee29ea7cb46150cb9e35a0201e8cc93c2ea20fd
unknown <shravanpitla180@gmail.com>
2026-09-23 11:24:15 +0530

    Operator

---
+++ Operator.py
@@ -0,0 +1,33 @@
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+
+#Assignment Operator
+#a=5
+a+=6
+print(a)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+#identity operatpr
+a=10
+b=10
+print(a is b) #single element
+
+a=[10,20,30]
+b=[10,20,30]
+print(a is not b)
+#membership opertaor
+list=[10,20,30,50,40]
+print(20 in list)
+print(10 not in list)
