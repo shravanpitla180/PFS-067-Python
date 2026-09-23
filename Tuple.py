86664731cfd7b6ce0910f887877089f8b2e1cfaf
unknown <shravanpitla180@gmail.com>
2026-09-23 10:47:58 +0530

    Tuple

---
+++ Tuple.py
@@ -0,0 +1,44 @@
+#Tuple
+student = (10 ,20 ,45,47,30)
+print(student)
+
+#slicing
+my_tuple=(10,20,30,40,50,)
+print(my_tuple[1:4])
+
+#set
+seen={10,20,20,30,33,40,40 }
+print(seen)
+
+#dictionary
+student={
+    "name":"shravan",
+    "age" : "21",
+    "place":"Hyd"
+}
+print(student["name"])
+print(student["age"])
+print(student["place"])
+
+student["name"]="shravn"
+print(student)
+
+#Type converstion
+age= int(input("enter your age:"))
+print(age)
+print(type(age))
+
+#convertin str to int
+x=100
+y=int(x)
+print(x)
+print(type(x))
+print(type(y))
+
+#converting int to float
+a=100
+b=float(a)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
